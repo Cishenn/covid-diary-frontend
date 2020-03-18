54d681d9401214fff2a80d9620d4dd5c350d4118
bigboss <1729117218@qq.com>
2020-03-18 21:51:20 +0800

    编写了首页部分样式 2020/3/18

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,28 @@ const app = getApp()
 
 Page({
   data: {
-    
+    showBGImage: true,
+    swiperList: [
+      {
+        _id: 0,
+        image_src: "/images/scroll_image1.jpg"
+      },
+      {
+        _id: 1,
+        image_src: "/images/scroll_image2.jpg"
+      },
+      {
+        _id: 2,
+        image_src: "/images/scroll_image3.jpg"
+      },
+    ]
   },
-  
+
   onLoad: function () {
-    
+    setTimeout(res => {
+      this.setData({
+        showBGImage: false
+      })
+    }, 1000)
   }
 })
