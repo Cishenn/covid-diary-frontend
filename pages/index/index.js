63cb8f64254c9a68738b68aa5274333d2f3f967d
bigboss <1729117218@qq.com>
2020-03-23 20:55:50 +0800

    add search page 2020/3/23

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,6 +6,7 @@ Page({
   data: {
     showBGImage: true,
     showSettings: false,
+    floorstatus: false,
     swiperList: [
       {
         _id: 0,
@@ -35,4 +36,29 @@ Page({
       showSettings: !this.data.showSettings
     });
   },
+
+  onPageScroll(e) {
+    if (e.scrollTop > 100) {
+      this.setData({
+        floorstatus: true
+      });
+    } else {
+      this.setData({
+        floorstatus: false
+      });
+    }
+  },
+
+  goPageTop(e) {
+    if (wx.pageScrollTo) {
+      wx.pageScrollTo({
+        scrollTop: 0
+      })
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+      })
+    }
+  }
 })
