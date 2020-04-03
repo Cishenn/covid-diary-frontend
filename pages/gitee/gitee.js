a6c64a86bf0d03356c51a8c46086b0f0e6d30596
ZZH23333 <3321403832@qq.com>
2020-04-04 03:08:21 +0800

    update pages/gitee/gitee.js.

--- pages/gitee/gitee.js
+++ pages/gitee/gitee.js
@@ -5,9 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    contents:'这是可以复制的文字,粘贴后即可看到效果'
   },
-
+  copyText: function (e) {
+    console.log(e)
+    wx.setClipboardData({
+      data: e.currentTarget.dataset.text,
+      success: function (res) {
+        wx.getClipboardData({
+          success: function (res) {
+            wx.showToast({
+              title: '复制成功'
+            })
+          }
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
